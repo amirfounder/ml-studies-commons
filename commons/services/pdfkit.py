500cfcf649264fdd8dd8195e655fb0601c7f0604
Amir Sharapov <amir@amirfounder.com>
2022-04-20 21:20:20 -0500

    removed arg

--- commons/services/pdfkit.py
+++ commons/services/pdfkit.py
@@ -4,8 +4,8 @@ import pdfkit
 DEFAULT_EXE_LOCATION = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 
 class PdfKit:
-    def __init__(self, path: str = DEFAULT_EXE_LOCATION):
-        self.configs = pdfkit.configuration(wkhtmltopdf=path)
+    def __init__(self):
+        self.configs = pdfkit.configuration(wkhtmltopdf=DEFAULT_EXE_LOCATION)
         self.pdfkit = pdfkit
 
     def from_file(self, path, *args, **kwargs):
