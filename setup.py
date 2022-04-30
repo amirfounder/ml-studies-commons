2a1b45379199da74d0e9a09996f7184385a75f91
Amir Sharapov <amir@amirfounder.com>
2022-04-30 18:24:53 -0500

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='commons',
-    version='0.0.2',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'pdfkit==1.0.0',
