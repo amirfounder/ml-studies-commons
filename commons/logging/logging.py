1ae66f11573078ff56823bd60cff5fb53913883b
Amir Sharapov <amir@amirfounder.com>
2022-05-01 09:39:39 -0500

    fix

--- commons/logging/logging.py
+++ commons/logging/logging.py
@@ -5,35 +5,33 @@ from daos import LogFile
 class Logger:
     def __init__(self, ml_studies_component: str):
         log_file = LogFile
-        log_file += "/" + ml_studies_component
+        log_file.path += "/" + ml_studies_component
 
     @classmethod
     def get_logger(cls, ml_studies_component: str):
         return cls(ml_studies_component)
 
     @staticmethod
-    def _build_message(s: str, _type: str, ):
+    def _build_message(s: str, level: str, ):
         msg = datetime.now().isoformat().ljust(30)
-        msg += _type.ljust(10)
+        msg += level.ljust(10)
         msg += s.ljust(100) if len(s) <= 100 else s[:97] + '...'
         return msg
 
-    def success(self, message: str):
-        message = self._build_message(message, 'SUCCESS')
+    def log(self, message: str, level: str, build_message=True):
+        if build_message:
+            message = self._build_message(message, level)
         print(message)
         LogFile.log(message)
+
+    def success(self, message: str, build_message=True):
+        self.log(message, 'SUCCESS')
 
     def info(self, message: str):
-        message = self._build_message(message, 'INFO')
-        print(message)
-        LogFile.log(message)
+        self.log(message, 'INFO')
 
     def warning(self, message: str):
-        message = self._build_message(message, 'WARNING')
-        print(message)
-        LogFile.log(message)
+        self.log(message, 'WARNING')
 
     def error(self, message: str):
-        message = self._build_message(message, 'ERROR')
-        print(message)
-        LogFile.log(message)
+        self.log(message, 'ERROR')
